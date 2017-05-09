12ce0311ac3f32a8fc9f45fdb12d55ce6966224c
David Gomez <dg.dave@gmail.com>
2017-05-09 15:25:35 -0400

    Write HEX color validation

--- __tests__/src/validate.test.js
+++ __tests__/src/validate.test.js
@@ -18,3 +18,28 @@ test('Is not a valid Size', () => {
   expect(validate('1920xx1080', 'size')).toBe(false)
   expect(validate('800x1280x1920', 'size')).toBe(false)
 })
+
+test('Is a valid HEX color value (shorthand)', () => {
+  expect(validate('eee', 'color')).toBe(true)
+  expect(validate('e48', 'color')).toBe(true)
+  expect(validate('cde', 'color')).toBe(true)
+})
+
+test('Is not a valid HEX color value (shorthand)', () => {
+  expect(validate('ee', 'color')).toBe(false)
+  expect(validate('e48d', 'color')).toBe(false)
+  expect(validate('cge', 'color')).toBe(false)
+})
+
+test('Is a valid HEX color value', () => {
+  expect(validate('efefef', 'color')).toBe(true)
+  expect(validate('aeb4c8', 'color')).toBe(true)
+  expect(validate('c4d4e4', 'color')).toBe(true)
+})
+
+test('Is not a valid HEX color value', () => {
+  expect(validate('efef', 'color')).toBe(false)
+  expect(validate('aeb4c', 'color')).toBe(false)
+  expect(validate('c4d4e4f', 'color')).toBe(false)
+  expect(validate('c4g4e4', 'color')).toBe(false)
+})
--- src/validate.js
+++ src/validate.js
@@ -1,11 +1,17 @@
-export const isValidSize = input => {
+const isValidSize = input => {
   const widthRe = /^\d+$/
   const fullSizeRe = /^\d+x\d+$/
   return widthRe.test(input) || fullSizeRe.test(input)
 }
 
+const isValidColor = input => {
+  const colorRe = /^(([0-9a-fA-F]{2}){3}|([0-9a-fA-F]){3})$/
+  return colorRe.test(input)
+}
+
 const validationMap = {
-  size: isValidSize
+  size: isValidSize,
+  color: isValidColor
 }
 
 export default (value, type) => {
