ce85cdd2b910b2ebae0df92ba9b960e00b2d1428
David Gomez <dg.dave@gmail.com>
2017-05-16 14:14:33 -0400

    Refactor code for simplicity

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const validTypes = ['gif', 'jpg', 'png']
+
+export const types = [
+  { value: 'gif', label: 'gif' },
+  { value: 'jpg', label: 'jpg' },
+  { value: 'png', label: 'png' }
+]
+
+export const filters = [
+  { value: 'lark', label: 'Lark'},
+  { value: 'reyes', label: 'Reyes'},
+  { value: 'juno', label: 'Juno'},
+  { value: 'slumber', label: 'Slumber'},
+  { value: 'crema', label: 'Crema'},
+  { value: 'ludwig', label: 'Ludwig'},
+  { value: 'aden', label: 'Aden'},
+  { value: 'perpetua', label: 'Perpetua'},
+  { value: 'amaro', label: 'Amaro'},
+  { value: 'mayfair', label: 'Mayfair'},
+  { value: 'rise', label: 'Rise'},
+  { value: 'hudson', label: 'Hudson'},
+  { value: 'valencia', label: 'Valencia'},
+  { value: 'xpro2', label: 'Xpro2'},
+  { value: 'sierra', label: 'Sierra'},
+  { value: 'willow', label: 'Willow'},
+  { value: 'lofi', label: 'Lofi'},
+  { value: 'earlybird', label: 'Earlybird'},
+  { value: 'brannan', label: 'Brannan'},
+  { value: 'inkwell', label: 'Inkwell'},
+  { value: 'hefe', label: 'Hefe'},
+  { value: 'nashville', label: 'Nashville'},
+  { value: 'sutro', label: 'Sutro'},
+  { value: 'toaster', label: 'Toaster'},
+  { value: 'walden', label: 'Walden'},
+  { value: 'nineteenSeventySeven', label: 'NineteenSeventySeven'},
+  { value: 'kelvin', label: 'Kelvin'}
+]
