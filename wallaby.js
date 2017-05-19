905fab6b6cd02a47aa05c6e5f60adca474934862
David Gomez <dg.dave@gmail.com>
2017-05-19 16:02:09 -0400

    Tweak Wallaby.js config file

--- wallaby.js
+++ wallaby.js
@@ -2,11 +2,16 @@ module.exports = function (wallaby) {
   return {
     files: [
       'src/**/*.js',
-      'pages/**/*.js',
+      '__tests__/src/test-utils.js',
       '!__tests__/**/*.test.js'
     ],
 
     tests: ['__tests__/**/*.test.js'],
+
+    filesWithNoCoverageCalculated: [
+      'src/data.js',
+      '__tests__/src/test-utils.js'
+    ],
 
     compilers: {
       '**/*.js': wallaby.compilers.babel()
